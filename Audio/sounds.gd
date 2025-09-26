92d95aa55ab54258355b10fd0f0c4735568b2fbd
alexcolemandata <coleman.g.alex@gmail.com>
2025-09-26 17:55:15 +1000

    turn off reorder-code for formatter

--- Audio/sounds.gd
+++ Audio/sounds.gd
@@ -1,10 +1,13 @@
 class_name Sounds
 extends Resource
 
+@export_group("General")
 @export var loot_gold: SoundSettings
 @export var round_start: SoundSettings
+@export var town_celebrate: SoundSettings
+
+@export_group("Tower", "tower_")
 @export var tower_attack_beam_light: SoundSettings
 @export var tower_attack_fireball_explosion: SoundSettings
 @export var tower_attack_fireball_shoot: SoundSettings
 @export var tower_level_up: SoundSettings
-@export var town_celebrate: SoundSettings
