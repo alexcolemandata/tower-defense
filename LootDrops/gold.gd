41f9642435350cce850afd00ce0020687110e0d6
alexcolemandata <coleman.g.alex@gmail.com>
2025-10-02 22:54:01 +1000

    gold attraction fixes - this time it works i swear

--- LootDrops/gold.gd
+++ LootDrops/gold.gd
@@ -5,11 +5,12 @@ signal collected(money: int)
 
 var is_collected: bool = false
 var is_attracted_to_mouse: bool = false
+var is_mouse_attractable: bool = true
 var money_value: int = 10
 var current_velocity := Vector2.ZERO
+var current_accel := Vector2.ZERO
 
 @export var max_speed: float = 200.0
-@export var max_accel: float = 800000000.0
 @export var inertia_weight: float = 0.1
 
 @onready var mouse_over_area: Area2D = $MouseOverArea
@@ -27,26 +28,35 @@ func collect_loot():
 	queue_free()
 
 func _ready() -> void:
-	scale = Vector2.ONE * clamp(money_value / 10.0, 0.4, 3.0)
+	scale = Vector2.ONE * 0.5 + Vector2.ONE * clamp(money_value / 50.0, 0.0, 3.0)
 
 func _on_mouse_over_area_mouse_entered() -> void:
 	if is_collected:
 		return
 	collect_loot()
 
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_WM_MOUSE_EXIT:
+			is_mouse_attractable = false
+		NOTIFICATION_WM_MOUSE_ENTER:
+			is_mouse_attractable = true
+
 func _on_mouse_attract_area_mouse_entered() -> void:
 	is_attracted_to_mouse = true
 
 func _physics_process(delta: float) -> void:
-	if not is_attracted_to_mouse:
+	if not is_attracted_to_mouse and is_mouse_attractable:
 		return
 	
+	current_velocity += current_accel * delta
 	global_position += current_velocity * delta
 	
 	var vec_to_mouse: Vector2 = (get_global_mouse_position() - global_position)
 	var weighted_mouse_vector = vec_to_mouse.normalized() * max_speed * (
-		(mouse_attract_shape.shape.radius - vec_to_mouse.length()) / (mouse_attract_shape.shape.radius)
+		clamp(mouse_attract_shape.shape.radius - vec_to_mouse.length(), 0, max_speed) / (mouse_attract_shape.shape.radius)
 	)
 	
 	current_velocity = lerp(current_velocity, weighted_mouse_vector, inertia_weight)
+
 	return
